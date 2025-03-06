0edc28677cefbbf1a0fed4ecd498630ebf66c47d
DHeReign <varnik@icloud.com>
2025-03-06 19:47:14 +0300

    max

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const canvas = document.querySelector('#game');
-const context = canvas.getContext('2d');
+const map = document.querySelector('#game');
+const canvas = map.getContext('2d');
+canvas.fillStyle = 'rgb(228, 164, 87)';
+
 const grid = 15;
 const paddleHeight = grid * 5;
-const maxPaddleY = canvas.height - grid - paddleHeight;
+const maxPaddleY = map.height - grid - paddleHeight;
 
 const leftCounter = document.querySelector('#leftCounter');
 const rightCounter = document.querySelector('#rightCounter');
@@ -10,37 +12,88 @@ const rightCounter = document.querySelector('#rightCounter');
 leftCounter.textContent = 0;
 rightCounter.textContent = 0;
 
-let ballSpeed = 5;
-let paddleSpeed = 6;
+
+let ballSpeed = 7;
+let paddleSpeed = 7;
 
 const leftPaddle = {
     x: grid * 2,
-    y: canvas.height / 2 - paddleHeight / 2,
+    y: map.height / 2 - paddleHeight / 2,
     width: grid,
     height: paddleHeight,
     dy: 0,
 }
 
 const rightPaddle = {
-    x: canvas.width - grid * 3,
-    y: canvas.height / 2 - paddleHeight / 2,
+    x: map.width - grid * 3,
+    y: map.height / 2 - paddleHeight / 2,
     width: grid,
     height: paddleHeight,
     dy: 0,
 }
 
 const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
+    x: map.width / 2,
+    y: map.height / 2,
     width: grid,
     height: grid,
-    resetting: false,
     dx: ballSpeed,
     dy: -ballSpeed,
-    isResetted: false
+    isResetted: false,
 }
 
-function collidesWallsByPaddles(paddle) {
+function renderMap() {
+    canvas.fillRect(0, 0, map.width, grid); // Верхняя граница
+    canvas.fillRect(0, map.height - grid, map.width, grid) // Нижняя граница
+
+    for (let i = grid; i < map.height - grid; i += grid * 2) {
+        canvas.fillRect(map.width / 2, i, grid, grid); // Разделительная линия
+    }
+}
+
+function clearMap() {
+    canvas.clearRect(0, 0, map.width, map.height);
+}
+
+function renderLeftPaddle() {
+    canvas.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+}
+
+function renderRightPaddle() {
+    canvas.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+}
+
+function movePaddles() {
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+}
+
+function renderBall() {
+    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+function resetGame() {
+    if ((ball.x < 0 || ball.x > map.width) && !ball.isResetted) {
+        if (ball.x < 0) {
+            leftCounter.textContent = 0;
+        } else if (ball.x > map.width) {
+            rightCounter.textContent = 0;
+        }
+        ball.isResetted = true;
+        setTimeout(() => {
+            ball.x = map.width / 2;
+            ball.y = map.height / 2;
+            ball.isResetted = false;
+        }, 1000);
+    }
+}
+
+function collideWallsWithPaddle(paddle) {
     if (paddle.y < grid) {
         paddle.y = grid;
     }
@@ -49,98 +102,85 @@ function collidesWallsByPaddles(paddle) {
     }
 }
 
-function collideVerticalWalls() {
+function collideWallsWithPaddles() {
+    collideWallsWithPaddle(leftPaddle);
+    collideWallsWithPaddle(rightPaddle);
+}
+
+function collideWallsWithBall() {
     if (ball.y < grid) {
         ball.y = grid;
         ball.dy = -ball.dy;
     }
-    else if (ball.y > canvas.height - grid) {
-        ball.y = canvas.height - grid;
+    else if (ball.y > map.height - grid) {
+        ball.y = map.height - grid;
         ball.dy = -ball.dy;
     }
 }
 
-function collidesPaddles() {
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+
+function collidePaddlesWithBall() {
     if (isCollides(ball, rightPaddle)) {
         ball.dx = -ball.dx;
         ball.x = rightPaddle.x - ball.width;
-        let number = Number(rightCounter.textContent) + 1;
-        rightCounter.textContent = number;
+        rightCounter.textContent = parseInt(rightCounter.textContent) + 1;
     }
     else if (isCollides(ball, leftPaddle)) {
         ball.dx = -ball.dx;
         ball.x = leftPaddle.x + leftPaddle.width;
-        let number = Number(leftCounter.textContent) + 1;
-        leftCounter.textContent = number;
+        leftCounter.textContent = parseInt(leftCounter.textContent) + 1;
     }
 }
 
-function isCollides(object1, object2) {
-    return object1.x < object2.x + object2.width
-        && object1.x + object1.width > object2.x
-        && object1.y < object2.y + object2.height
-        && object1.y + object1.height > object2.y;
-}
+function aiControl() {
+    let direction = 0;
 
-function resetGame() {
-    if ((ball.x < 0 || ball.x > canvas.width) && !ball.isResetted) {
-        if (ball.x < 0) {
-            leftCounter.textContent = 0;
-        } else if (ball.x > canvas.width) {
-            rightCounter.textContent = 0;
+    if (ball.x > Math.random() * map.width / 2) {
+        if (ball.y < rightPaddle.y) {
+            direction = -1;
         }
-        ball.isResetted = true;
-        setTimeout(function () {
-            ball.x = canvas.width / 2,
-                ball.y = canvas.height / 2,
-                ball.isResetted = false
-        }, 3000);
+        else if (ball.y > rightPaddle.y + paddleHeight) {
+            direction = 1;
+        }
+
+        rightPaddle.y += paddleSpeed * direction;
     }
 }
 
-function moveBall() {
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-}
-
-function movePaddles() {
-    leftPaddle.y += leftPaddle.dy;
-    rightPaddle.y += rightPaddle.dy;
-}
 
 function loop() {
-    requestAnimationFrame(loop);
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    clearMap();
 
+    renderLeftPaddle();
+    renderRightPaddle();
+    aiControl(); // 24
     movePaddles();
 
-    collidesWallsByPaddles(leftPaddle);
-    collidesWallsByPaddles(rightPaddle);
+    collideWallsWithPaddles(); // 24 
 
-    context.fillStyle = 'rgb(228, 164, 87)';
-    context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);  //Left Paddle
-    context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height); //Right Paddle
-
+    renderBall();
     moveBall();
 
-    resetGame();
+    collideWallsWithBall();   // 24 
+    collidePaddlesWithBall(); // 24 
 
-    collideVerticalWalls();
-
-    collidesPaddles();
-
-    context.fillRect(ball.x, ball.y, ball.width, ball.height); //Ball
-
-    context.fillRect(0, 0, canvas.width, grid); // Wall Top
-    context.fillRect(0, canvas.height - grid, canvas.width, grid) // Wall Bottom
-
-    for (let i = grid; i < canvas.height - grid; i += grid * 2) {
-        context.fillRect(canvas.width / 2, i, grid, grid); //Net Line
-    }
+    resetGame(); // 24
+    renderMap();
+    requestAnimationFrame(loop);
 }
 
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', (event) => {
     if (event.key === 'w' || event.key === 'ц') {
         leftPaddle.dy = -paddleSpeed;
     }
@@ -148,24 +188,11 @@ document.addEventListener('keydown', function (event) {
         leftPaddle.dy = paddleSpeed;
     }
 });
-document.addEventListener('keyup', function (event) {
+document.addEventListener('keyup', (event) => {
     if (event.key === 'w' || event.key === 's' || event.key === 'ы' || event.key === 'ц') {
         leftPaddle.dy = 0;
     }
 })
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowUp') {
-        rightPaddle.dy = -paddleSpeed;
-    }
-    else if (event.key === 'ArrowDown') {
-        rightPaddle.dy = paddleSpeed;
-    }
-});
-document.addEventListener('keyup', function (event) {
-    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-        rightPaddle.dy = 0;
-    }
-})
 
 requestAnimationFrame(loop);
