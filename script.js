5639acf3e1be7306a1cec70d5436a9ccbf414516
DHeReign <varnik@icloud.com>
2025-03-06 18:39:38 +0300

    final

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
-const map = document.querySelector("game");
-const canvas = map.getContext('2d');
-canvas.fillStyle = "rgb (228, 164, 87)";
-
-
+const canvas = document.querySelector('#game');
+const context = canvas.getContext('2d');
 const grid = 15;
 const paddleHeight = grid * 5;
-const maxPaddlyY = map.height - grid - paddleHeight;
+const maxPaddleY = canvas.height - grid - paddleHeight;
+
+const leftCounter = document.querySelector('#leftCounter');
+const rightCounter = document.querySelector('#rightCounter');
+
+leftCounter.textContent = 0;
+rightCounter.textContent = 0;
 
 let ballSpeed = 5;
-let paddleSpeed = 7;
+let paddleSpeed = 6;
 
 const leftPaddle = {
     x: grid * 2,
-    y: map.height / 2 - paddleHeight / 2, 
-    width: grid, 
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
     height: paddleHeight,
-    dy: 0, // Скорость перемещения по оси Y
+    dy: 0,
 }
 
 const rightPaddle = {
-    x: map.width - grid * 3,
-    y: map.height / 2 - paddleHeight / 2, 
-    width: grid, 
+    x: canvas.width - grid * 3,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: grid,
     height: paddleHeight,
-    dy: 0, // Скорость перемещения по оси Y
+    dy: 0,
 }
 
 const ball = {
-    x: map.width / 2,
-    y: map.height / 2,
+    x: canvas.width / 2,
+    y: canvas.height / 2,
     width: grid,
     height: grid,
+    resetting: false,
     dx: ballSpeed,
     dy: -ballSpeed,
-    resetting: false,
-    inResetted: false,
+    isResetted: false
 }
 
-
-
-function renderMap() {
-    canvas.fillRect(0, 0, map.width, grid); // Верхняя граница
-    canvas.fillRect(0, map.height - grid, map.width, grid) // Нижняя граница
-
-    for (let i = grid; i < map.height - grid; i += grid * 2) {
-        canvas.fillRect(map.width / 2, i, grid, grid); // Разделительная линия
+function collidesWallsByPaddles(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
     }
 }
 
-function clearMap() {
-    canvas.clearRect(0, 0, map.width, map.height);
+function collideVerticalWalls() {
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy = -ball.dy;
+    }
+    else if (ball.y > canvas.height - grid) {
+        ball.y = canvas.height - grid;
+        ball.dy = -ball.dy;
+    }
 }
 
-function loop() {
-    clearMap();
-
-    renderLeftPaddle();
-    renderRightPaddle();
-
-    movePaddles();
-
-    renderBall();
-
-    renderMap();
-    requestAnimationFrame(loop);
+function collidesPaddles() {
+    if (isCollides(ball, rightPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = rightPaddle.x - ball.width;
+        let number = Number(rightCounter.textContent) + 1;
+        rightCounter.textContent = number;
+    }
+    else if (isCollides(ball, leftPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = leftPaddle.x + leftPaddle.width;
+        let number = Number(leftCounter.textContent) + 1;
+        leftCounter.textContent = number;
+    }
 }
 
-function renderBall() {
-    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
+function isCollides(object1, object2) {
+    return object1.x < object2.x + object2.width
+        && object1.x + object1.width > object2.x
+        && object1.y < object2.y + object2.height
+        && object1.y + object1.height > object2.y;
+}
+
+function resetGame() {
+    if ((ball.x < 0 || ball.x > canvas.width) && !ball.isResetted) {
+        if (ball.x < 0) {
+            leftCounter.textContent = 0;
+        } else if (ball.x > canvas.width) {
+            rightCounter.textContent = 0;
+        }
+        ball.isResetted = true;
+        setTimeout(function () {
+            ball.x = canvas.width / 2,
+                ball.y = canvas.height / 2,
+                ball.isResetted = false
+        }, 3000);
+    }
 }
 
 function moveBall() {
@@ -75,17 +103,68 @@ function moveBall() {
     ball.y += ball.dy;
 }
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'w' || event.key === "ц") {
+function movePaddles() {
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    movePaddles();
+
+    collidesWallsByPaddles(leftPaddle);
+    collidesWallsByPaddles(rightPaddle);
+
+    context.fillStyle = 'rgb(228, 164, 87)';
+    context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);  //Left Paddle
+    context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height); //Right Paddle
+
+    moveBall();
+
+    resetGame();
+
+    collideVerticalWalls();
+
+    collidesPaddles();
+
+    context.fillRect(ball.x, ball.y, ball.width, ball.height); //Ball
+
+    context.fillRect(0, 0, canvas.width, grid); // Wall Top
+    context.fillRect(0, canvas.height - grid, canvas.width, grid) // Wall Bottom
+
+    for (let i = grid; i < canvas.height - grid; i += grid * 2) {
+        context.fillRect(canvas.width / 2, i, grid, grid); //Net Line
+    }
+}
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'w' || event.key === 'ц') {
         leftPaddle.dy = -paddleSpeed;
     }
-    else if (event.key === 's' || event.key === "ы") {
+    else if (event.key === 's' || event.key === 'ы') {
         leftPaddle.dy = paddleSpeed;
     }
 });
-document.addEventListener('keyup', (event) => {
-    if (event.key === 'w' || event.key === 's' || event.key === "ц" || event.key === "ы") {
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'w' || event.key === 's' || event.key === 'ы' || event.key === 'ц') {
         leftPaddle.dy = 0;
+    }
+})
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowUp') {
+        rightPaddle.dy = -paddleSpeed;
+    }
+    else if (event.key === 'ArrowDown') {
+        rightPaddle.dy = paddleSpeed;
+    }
+});
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        rightPaddle.dy = 0;
     }
 })
 
